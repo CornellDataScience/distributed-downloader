fb5997a21b0bd38fda370e4d2f4fa01f8989d5a8
Naijei <nj277@cornell.edu>
2026-02-28 11:59:04 -0500

    Week 2, Peer and Tracker basics

--- peer/src/main/java/cds/distdownloader/peer/PeerGrpcService.java
+++ peer/src/main/java/cds/distdownloader/peer/PeerGrpcService.java
@@ -1,23 +1,79 @@
 package cds.distdownloader.peer;
 
 import cds.distdownloader.proto.ChunkBitmap;
+import cds.distdownloader.proto.ChunkRef;
 import cds.distdownloader.proto.ChunkRequest;
 import cds.distdownloader.proto.ChunkResponse;
 import cds.distdownloader.proto.FileRequest;
 import cds.distdownloader.proto.PeerGrpc;
+import com.google.protobuf.ByteString;
 import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
+import java.io.ByteArrayOutputStream;
 import org.springframework.stereotype.Service;
+import java.util.Map;
+import java.util.HashMap;
 
 @Service
-public class PeerGrpcService extends PeerGrpc.PeerImplBase {
+public class PeerGrpcService extends PeerGrpc.PeerImplBase { //"Test.bin", 10, 1 2 4 // -> 0000010110
+    //filename -> (chunkbit -> bytes)
+    Map<String, Map<Integer, ByteString>> fileToChunk = new HashMap<>();
+
     @Override
+    //receive FileRequest from client. send back chunkBitmap
     public void getAvailability(FileRequest request, StreamObserver<ChunkBitmap> responseObserver) {
-        responseObserver.onError(Status.UNIMPLEMENTED.asRuntimeException());
+        String fileName = request.getFileId();
+        ChunkBitmap newBitMap;
+        int chunks = 10; //ASSUME EACH FILE HAS 10 Chunks
+        //PeerEndpoint newPeer = PeerEndpoint.newBuilder().setId("1").setIp("2").setPort(123).build();
+        //For now assume we only have file "Test.bin", any other file request should be rejected
+        if (!fileName.startsWith("Test.bin")) {
+            ByteString value = ByteString.EMPTY;
+            newBitMap = ChunkBitmap.newBuilder().setFileId(fileName).setNumChunks(chunks).setBitset(value).build();
+        }
+        else{
+            Map<Integer, ByteString> chunkMap = fileToChunk.getOrDefault(fileName, Map.of());
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+            int currentByte = 0;
+            int bitCount = 0;
+
+            for (int i = 0; i < chunks; i++) {
+                int idx = chunks - i - 1; // chunk (chunks-1) ... chunk 0
+                int bit = chunkMap.containsKey(idx) ? 1 : 0;
+
+                currentByte = (currentByte << 1) | bit;  // shift then add bit
+                bitCount++;
+
+                if (bitCount == 8) {
+                    out.write(currentByte);
+                    currentByte = 0;
+                    bitCount = 0;
+                }
+            }
+            if (bitCount > 0) {
+                currentByte <<= (8 - bitCount); // pad remaining bits on the right
+                out.write(currentByte);
+            }
+
+            ByteString value = ByteString.copyFrom(out.toByteArray());
+            newBitMap = ChunkBitmap.newBuilder().setFileId(fileName).setNumChunks(0).setBitset(value).build();
+        }
+
+        responseObserver.onNext(newBitMap);
+        responseObserver.onCompleted();
     }
 
     @Override
     public void getChunk(ChunkRequest request, StreamObserver<ChunkResponse> responseObserver) {
-        responseObserver.onError(Status.UNIMPLEMENTED.asRuntimeException());
+        ChunkRef chunk = request.getChunk();
+        String file = chunk.getFileId();
+        Integer index = chunk.getChunkIndex();
+        ByteString chunkBytes = fileToChunk.get(file).get(index);
+        ChunkResponse resp = ChunkResponse.newBuilder()
+                .setData(chunkBytes)
+                .build();
+        responseObserver.onNext(resp);
+        responseObserver.onCompleted();
     }
 }
--- tracker/src/main/java/cds/distdownloader/tracker/TrackerGrpcService.java
+++ tracker/src/main/java/cds/distdownloader/tracker/TrackerGrpcService.java
@@ -3,26 +3,53 @@ package cds.distdownloader.tracker;
 import cds.distdownloader.proto.*;
 import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
+import java.util.HashSet;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.time.Instant;
 
 @Service
 public class TrackerGrpcService extends TrackerGrpc.TrackerImplBase {
 
-    @Override
-    public void register(RegisterRequest request,
-            StreamObserver<RegisterResponse> responseObserver) {
-        responseObserver.onError(Status.UNIMPLEMENTED.asRuntimeException());
-    }
+    // https://www.geeksforgeeks.org/java/java-time-instant-class-in-java/
+    // who is alive; peer -> last seen
+    private Map<PeerEndpoint, Instant> peerMap = new HashMap<>();
 
+    //Peer will send a heart beat to tracker(5s) , if no heart beat is seen in the last 10 second delete them
     @Override
-    public void heartbeat(HeartbeatRequest request,
+    public synchronized void heartbeat(HeartbeatRequest request,
             StreamObserver<HeartbeatResponse> responseObserver) {
-        responseObserver.onError(Status.UNIMPLEMENTED.asRuntimeException());
+        peerMap.put(request.getEndpoint(), Instant.now());
+
+        HeartbeatResponse resp = HeartbeatResponse.newBuilder()
+                .setAck(Ack.newBuilder().setOk(true).build()).build();
+        responseObserver.onNext(resp);
+        responseObserver.onCompleted();
     }
 
     @Override
-    public void listPeers(ListPeersRequest request,
+    /**
+     * Loop through all peers in the map, and delete all that were 10+ seconds
+     * from the current instant.
+     * Then, we will send out a list version of the keySet
+     */
+    public synchronized void listPeers(ListPeersRequest request,
             StreamObserver<ListPeersResponse> responseObserver) {
-        responseObserver.onError(Status.UNIMPLEMENTED.asRuntimeException());
+        Set<PeerEndpoint> peers = new HashSet<>(peerMap.keySet());
+
+        Instant timeNow = Instant.now();
+        Instant cutoff = timeNow.minusSeconds(10);
+        for (PeerEndpoint peer : peers) {
+            Instant lastSeen = peerMap.get(peer);
+            if (lastSeen.isBefore(cutoff)) {
+                peerMap.remove(peer);
+            }
+        }
+
+        List<PeerEndpoint> peerList = new ArrayList<>(peerMap.keySet());
+        ListPeersResponse response = ListPeersResponse.newBuilder().addAllUpPeers(peerList).build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
     }
 }
