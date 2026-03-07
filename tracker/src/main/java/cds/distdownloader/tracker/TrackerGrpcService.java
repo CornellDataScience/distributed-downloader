f940762389f2118b934ff24468f428ac5906bda3
Skai Nzeuton <san82@cornell.edu>
2026-03-07 02:25:44 -0500

    Add peerToFiles map, fileToPeers map, GetPeersByFile()

--- tracker/src/main/java/cds/distdownloader/tracker/TrackerGrpcService.java
+++ tracker/src/main/java/cds/distdownloader/tracker/TrackerGrpcService.java
@@ -14,13 +14,32 @@ public class TrackerGrpcService extends TrackerGrpc.TrackerImplBase {
 
     // https://www.geeksforgeeks.org/java/java-time-instant-class-in-java/
     // who is alive; peer -> last seen
+    private Map<String, PeerEndpoint> peerToEndpoint = new HashMap<>();
     private Map<PeerEndpoint, Instant> peerMap = new HashMap<>();
+    private Map<String, Set<String>> peerToFiles = new HashMap<>();
+    private Map<String, Set<String>> fileToPeers = new HashMap<>();
 
     //Peer will send a heart beat to tracker(5s) , if no heart beat is seen in the last 10 second delete them
     @Override
     public synchronized void heartbeat(HeartbeatRequest request,
             StreamObserver<HeartbeatResponse> responseObserver) {
-        peerMap.put(request.getEndpoint(), Instant.now());
+        PeerEndpoint peerEndPoint = request.getEndpoint();
+        peerMap.put(peerEndPoint, Instant.now());
+
+        String peer_id = peerEndPoint.getId();
+        Set<String> file_ids = new HashSet<String>(request.getFileIdsList());
+
+        // add peer to peerToEndpoint
+        peerToEndpoint.put(peer_id, peerEndPoint);
+
+        // add files to peerToFiles
+        peerToFiles.put(peer_id, file_ids);
+
+        // add peer to fileToPeers
+        for (String file_id : file_ids) {
+            Set<String> filePeers = fileToPeers.getOrDefault(file_id, new HashSet<String>());
+            filePeers.add(peer_id);
+        }
 
         HeartbeatResponse resp = HeartbeatResponse.newBuilder()
                 .setAck(Ack.newBuilder().setOk(true).build()).build();
@@ -51,5 +70,20 @@ public class TrackerGrpcService extends TrackerGrpc.TrackerImplBase {
         ListPeersResponse response = ListPeersResponse.newBuilder().addAllUpPeers(peerList).build();
         responseObserver.onNext(response);
         responseObserver.onCompleted();
+    }
+
+    private synchronized List<String> GetPeersByFile(String fileId) {
+        List<String> peers = new ArrayList<String>();
+
+        Instant timeNow = Instant.now();
+        Instant cutoff = timeNow.minusSeconds(10);
+        for (String peerId : fileToPeers.get(fileId)) {
+            PeerEndpoint endpoint = peerToEndpoint.get(peerId);
+            if (peerMap.get(endpoint).isBefore(cutoff)) {
+                peers.add(peerId);
+            }
+        }
+
+        return peers;
     }
 }
